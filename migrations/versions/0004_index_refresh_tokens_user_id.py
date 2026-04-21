faa43f086d3bfe9c8ff1201e3cfa7cdf5bc73fcd
“vulpineAlicia“ <vulpinealicia@gmail.com>
2026-04-21 13:33:44 +0400

    Add index on refresh_tokens.user_id

---
+++ migrations/versions/0004_index_refresh_tokens_user_id.py
@@ -0,0 +1,23 @@
+"""index refresh_tokens.user_id
+
+Revision ID: 0004
+Revises: 0003
+Create Date: 2026-04-19
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+
+revision: str = "0004"
+down_revision: Union[str, None] = "0003"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_index("ix_refresh_tokens_user_id", "refresh_tokens", ["user_id"])
+
+
+def downgrade() -> None:
+    op.drop_index("ix_refresh_tokens_user_id", table_name="refresh_tokens")
